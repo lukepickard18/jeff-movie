bd6c1e693b3fe4c7f5f35894aad540e5a8af6d13
Luke Pickard <lukepc445@gmail.com>
2022-11-02 16:30:39 +1300

    After messing around with a bunch of different ways of css I just am starting with the built in in-line styling

--- luke-movie/src/components/Header.js
+++ luke-movie/src/components/Header.js
@@ -9,37 +9,80 @@ import LiveTvIcon from '@mui/icons-material/LiveTv';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import SearchIcon from '@mui/icons-material/Search';
 import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import { ForwardRef } from 'react';
+import { createUseStyles } from "react-jss";
+import { makeStyles } from '@mui/material';
+
+import './Header.css';
 
 export default function Header() {
+    const classes = useStyles();
+    
     return (
+        <div className={classes.root}>
         <AppBar position="static">
-        <Toolbar>
-            <HomeIcon
-            size="xlarge"
-            edge="start">
-            <HomeIcon />
-            </HomeIcon>
-            <FlashOnIcon
-            size="xlarge">
-            <FlashOnIcon />
-            </FlashOnIcon>
-            <LiveTvIcon
-            size="xlarge">
-            <LiveTvIcon />
-            </LiveTvIcon>
-            <PersonOutlineIcon
-            size="xlarge">
-            <PersonOutlineIcon />
-            </PersonOutlineIcon>
-            <SearchIcon
-            size="xlarge">
-            <SearchIcon />
-            </SearchIcon>
-            <VideoLibraryIcon
-            size="xlarge">
-            <VideoLibraryIcon />
-            </VideoLibraryIcon>
-        </Toolbar>
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <HomeIcon />
+            </IconButton>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <FlashOnIcon />
+            </IconButton>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <LiveTvIcon />
+            </IconButton>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <VideoLibraryIcon />
+            </IconButton>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <SearchIcon />
+            </IconButton>
+            </Toolbar>
         </AppBar>
+        </div>
     );
     }
+
+const useStyles = createUseStyles({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: 2,
+    },
+    title: {
+        flexGrow: 1,
+    },
+});
+
+    
