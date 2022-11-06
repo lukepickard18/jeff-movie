b27f0917374399b05b6d46ee4ef7515706e222cb
Luke Pickard <lukepc445@gmail.com>
2022-11-07 12:23:06 +1300

    styling - implemented usestyles to style the cards into rows instead of just one

--- luke-movie/src/components/Dashboard.js
+++ luke-movie/src/components/Dashboard.js
@@ -2,12 +2,13 @@ import MovieCard from "./MovieCard";
 import instance from "../utils/axios";
 import endpoints from "../utils/endpoints";
 import { useState, useEffect } from "react";
+import { createUseStyles } from "react-jss";
 
 
 
 export default function Dashboard() {
     const [movies, setMovies] = useState([]);
-    
+    const classes = useStyles();
     useEffect(() => {
         async function fetchData() {
         const req = await instance.get(endpoints[0].url);
@@ -18,11 +19,22 @@ export default function Dashboard() {
     }, []);
     
     return (
-        <div>
+        <div className={classes.movies}>
         {movies.map((movie) => (
             <MovieCard movie={movie} />
         ))}
         </div>
     );
     }
+    const useStyles = createUseStyles({
+        movies: {
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+            
+        },
+    });
+        
+            
+
 
